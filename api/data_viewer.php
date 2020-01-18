2162a011aa11b32b68956e7d97d4dcadb686c8d3
Leon Sievers <leon.sievers@tum.de>
2020-01-18 23:11:42 +0000

    Added temperature and visualizer

---
+++ api/data_viewer.php
@@ -0,0 +1,81 @@
+<?php
+//index.php
+$connect = mysqli_connect("localhost", "root", "abcd1234", "plant_measurements");
+$query = '
+SELECT light, 
+id AS datetime 
+FROM sensor_data
+';
+$result = mysqli_query($connect, $query);
+$rows = array();
+$table = array();
+
+$table['cols'] = array(
+ array(
+  'label' => 'Date Time', 
+  'type' => 'datetime'
+ ),
+ array(
+  'label' => 'Temperature (°C)', 
+  'type' => 'number'
+ )
+);
+
+while($row = mysqli_fetch_array($result))
+{
+ $sub_array = array();
+ $datetime = explode(".", $row["datetime"]);
+ $sub_array[] =  array(
+      "v" => 'Date(' . $datetime[0] . '000)'
+     );
+ $sub_array[] =  array(
+      "v" => $row["light"]
+     );
+ $rows[] =  array(
+     "c" => $sub_array
+    );
+}
+$table['rows'] = $rows;
+$jsonTable = json_encode($table);
+
+?>
+
+
+<html>
+ <head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript">
+   google.charts.load('current', {'packages':['corechart']});
+   google.charts.setOnLoadCallback(drawChart);
+   function drawChart()
+   {
+    var data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
+
+    var options = {
+     title:'Sensors Data',
+     legend:{position:'bottom'},
+     chartArea:{width:'95%', height:'65%'}
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
+
+    chart.draw(data, options);
+   }
+  </script>
+  <style>
+  .page-wrapper
+  {
+   width:1000px;
+   margin:0 auto;
+  }
+  </style>
+ </head>  
+ <body>
+  <div class="page-wrapper">
+   <br />
+   <h2 align="center">Display Google Line Chart with JSON PHP & Mysql</h2>
+   <div id="line_chart" style="width: 100%; height: 500px"></div>
+  </div>
+ </body>
+</html>
