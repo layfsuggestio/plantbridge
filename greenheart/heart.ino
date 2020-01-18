58345da05032c926924cc3b2bee362c704098598
Arne Sachtler <arne.sachtler@gmail.com>
2020-01-18 16:05:39 +0000

    Add REST call

--- greenheart/heart.ino
+++ greenheart/heart.ino
@@ -1,10 +1,3 @@
-/**
-   BasicHTTPClient.ino
-
-    Created on: 24.05.2015
-
-*/
-
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
@@ -14,63 +7,85 @@
 
 #include <WiFiClient.h>
 
+#define WIFI_OK_LED 0
+#define STATUS_LED 4
+#define RED 13
+
 ESP8266WiFiMulti WiFiMulti;
+
+String prefix = "http://104.198.192.55/api/"; ///new_measurement.php?humidity=55&light=0";
 
 void setup() {
 
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(WIFI_OK_LED, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(STATUS_LED, 0);
+  digitalWrite(WIFI_OK_LED, 0);
+  digitalWrite(RED, 0);
 
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("ArchNet", "TUbedefined");
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  digitalWrite(STATUS_LED, 1);
+
 }
 
 void loop() {
-  // wait for WiFi connection
+  // Set WifiStatus to STATUS_LED LED
+  digitalWrite(STATUS_LED, WiFi.status() == WL_CONNECTED);
+  
   if ((WiFiMulti.run() == WL_CONNECTED)) {
 
     WiFiClient client;
-
     HTTPClient http;
+    String endpoint = prefix + "request_status.php";
+    if (http.begin(client, endpoint)) {
+      int returnCode = http.GET();
 
-    Serial.print("[HTTP] begin...\n\r");
-    if (http.begin(client, "http://104.198.192.55/api/new_measurement.php?humidity=55&light=0")) {  // HTTP
-
-
-      Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = http.GET();
-
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = http.getString();
-          Serial.println(payload);
-        }
-      } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      if (returnCode == HTTP_CODE_OK) {
+        String message = http.getString();
+        Serial.printf("Setting WIFI_OK_LED LED to %d\n\r", message.toInt());
+        digitalWrite(WIFI_OK_LED, message.toInt());
       }
 
-      http.end();
-    } else {
-      Serial.printf("[HTTP} Unable to connect\n");
+      Serial.printf("ReturnCode: %d\n\r", returnCode);
     }
+    
+
+//    Serial.print("[HTTP] begin...\n\r");
+//    if (http.begin(client, "http://104.198.192.55/api/new_measurement.php?humidity=55&light=0")) {  // HTTP
+//
+//
+//      Serial.print("[HTTP] GET...\n");
+//      // start connection and send HTTP header
+//      int httpCode = http.GET();
+//
+//      // httpCode will be negative on error
+//      if (httpCode > 0) {
+//        // HTTP header has been send and Server response header has been handled
+//        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+//
+//        // file found at server
+//        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+//          String payload = http.getString();
+//          Serial.println(payload);
+//        }
+//      } else {
+//        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+//      }
+//
+//      http.end();
+//    } else {
+//      Serial.printf("[HTTP} Unable to connect\n");
+//    }
   }
 
-  delay(10000);
+  //delay(1000);
 }
