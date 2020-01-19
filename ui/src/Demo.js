f46d71efc4cdd389836a7a1801f685427e19ef5b
Florian Lay <florian.lay@gmx.de>
2020-01-19 08:00:35 +0000

    change pieces design

--- ui/src/Demo.js
+++ ui/src/Demo.js
@@ -1,5 +1,19 @@
 import React, { Component } from "react";
 import Chessboard from "chessboardjsx";
+import bB from "./imgs/bB.png";
+import bK from "./imgs/bK.png";
+import bN from "./imgs/bN.png";
+import bP from "./imgs/bP.png";
+import bQ from "./imgs/bQ.png";
+import bR from "./imgs/bR.png";
+
+import wB from "./imgs/wB.png";
+import wK from "./imgs/wK.png";
+import wN from "./imgs/wN.png";
+import wP from "./imgs/wP.png";
+import wQ from "./imgs/wQ.png";
+import wR from "./imgs/wR.png";
+
 // const io = require('socket.io-client/dist/socket.io');
 // // const socket = io("http://0.0.0.0:5000", {
 // //   transports: ['websocket']
@@ -52,6 +66,128 @@ class Demo extends Component {
             onDrop={() => {}}
             boardStyle={boardStyle}
             orientation="black"
+            pieces={{
+              wB: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wB}
+                  alt={"wB"}
+                />
+              ),
+              wK: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wK}
+                  alt={"wK"}
+                />
+              ),
+              wN: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wN}
+                  alt={"wN"}
+                />
+              ),
+              wP: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wP}
+                  alt={"wP"}
+                />
+              ),
+              wQ: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wQ}
+                  alt={"wQ"}
+                />
+              ),
+              wR: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={wR}
+                  alt={"wR"}
+                />
+              ),
+              bB: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bB}
+                  alt={"bB"}
+                />
+              ),
+              bK: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bK}
+                  alt={"bK"}
+                />
+              ),
+              bN: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bN}
+                  alt={"bN"}
+                />
+              ),
+              bP: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bP}
+                  alt={"bP"}
+                />
+              ),
+              bQ: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bQ}
+                  alt={"bQ"}
+                />
+              ),
+              bR: ({ squareWidth, isDragging }) => (
+                <img
+                  style={{
+                    width: isDragging ? squareWidth * 1.75 : squareWidth,
+                    height: isDragging ? squareWidth * 1.75 : squareWidth
+                  }}
+                  src={bR}
+                  alt={"bR"}
+                />
+              )
+            }}
           />
       </div>
     );
