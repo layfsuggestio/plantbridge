abd2049ee7b3166aeefb9e62474c4e2fa8c9783d
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2020-01-19 03:56:05 +0100

    add socketio

--- ui/src/Demo.js
+++ ui/src/Demo.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import Chessboard from "chessboardjsx";
-
-import StockFish from "./integrations/Stockfish.js";
+const io = require('socket.io-client/dist/socket.io');
+// const socket = io("http://0.0.0.0:5000", {
+//   transports: ['websocket']
+// });
+const socket = io("http://0.0.0.0:5000");
 
 class Demo extends Component {
+
+  state = {
+    position: "3B4/2b2pn1/P3p3/7p/1P1k3P/R4K1b/3P2r1/1N6"
+  };
+
+  componentDidMount() {
+    // Called when the board loads first
+    console.log("DID MOUNT");
+    socket.on("connection", () => console.log("al"));
+    socket.on("o", pos => console.log(pos));//this.haveNewPosition(pos));
+  }
+
   render() {
+    const { position } = this.state;
     return (
       <div style={boardsContainer}>
-        <StockFish>
-          {({ position, onDrop }) => (
-            <Chessboard
-              id="stockfish"
-              position={position}
-              width={window.innerWidth * 0.5}
-              onDrop={onDrop}
-              boardStyle={boardStyle}
-              orientation="black"
-            />
-          )}
-        </StockFish>
+          <Chessboard
+            id="board1"
+            position={position}
+            width={ window.innerWidth * 0.5}
+            onDrop={() => {}}
+            boardStyle={boardStyle}
+            orientation="black"
+          />
       </div>
     );
+  }
+
+  haveNewPosition(position) {
+    console.log(position);
+    this.setState({ position: position.fen });
   }
 }
 
